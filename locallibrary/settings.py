ee4cb1ef11e394dbf7233b85591b09eae5469da9
AndyHauge <curubethion@gmail.com>
2022-06-09 20:05:55 -0400

    Add heroku to allowed hosts

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ahauge-library.herokuapp.com','127.0.0.1']
 
 
 # Application definition
