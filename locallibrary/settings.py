b0a556e4c19a113411338c2af6e21e73bbbf84b2
AndyHauge <curubethion@gmail.com>
2022-06-11 10:17:29 -0400

    Added links to repo and self credit

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = TRUE
 
-ALLOWED_HOSTS = ['ahauge-library.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
